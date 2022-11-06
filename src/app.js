17d5373b408c9f1bf5565e18e04fac605abb1527
Alyssa Padayhag <alyssapadayhag@gmail.com>
2022-11-06 00:09:50 -0400

    added folders, cors, error handlers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const moviesRouter = require("./movies/movies.router");
+const notFound = require("./errors/routeNotFound");
+const errorHandler = require("./errors/routeErrorHandler");
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+
+// route not found and route error handler
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
