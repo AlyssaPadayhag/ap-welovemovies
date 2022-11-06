24896184b38618d7d14b8351159ecfc864a105da
Alyssa Padayhag <alyssapadayhag@gmail.com>
2022-11-06 01:41:08 -0500

    added GET /movies/:movieId/theaters route

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -3,7 +3,7 @@ const controller = require("./movies.controller");
 const theatersRouter = require("../theaters/theaters.router");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.use(":/movieId/theaters", controller.validateMovieId, theatersRouter);
+router.use("/:movieId/theaters", controller.validateMovieId, theatersRouter);
 
 router.route("/")
     .get(controller.list)
